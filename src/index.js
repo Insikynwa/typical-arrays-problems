5572997f1d799802cda6c11829cd7c0882cfb823
Insikynwa <pavel.churkin@mail.ru>
2021-03-07 14:15:02 +0300

    feat: implement min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
+exports.min = function min(array) {
+    if (array === undefined) {
+        return 0;
+    } else if (array.length !== 0) {
+        let numbMin = Math.min(...array);
+        return numbMin;
+    } else return 0;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array === undefined) {
+        return 0;
+    } else if (array.length !== 0) {
+        let numbMax = Math.max(...array);
+        return numbMax;
+    } else return 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array === undefined) {
+        return 0;
+    } else if (array.length !== 0) {
+        let numbAvg = array.reduce((prev, cur) => prev + cur) / array.length;
+        return numbAvg;
+    } else return 0;
+};
